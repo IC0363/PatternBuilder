4b2ca42199128243ee853b65cd449bf7255c4ad9
IC0363 <99269263+IC0363@users.noreply.github.com>
2024-03-14 20:25:33 -0400

    w

--- Pattern Builder/Pattern.java
+++ Pattern Builder/Pattern.java
@@ -25,30 +25,176 @@ public class Pattern
 
     // YOUR METHODS (for problems 1 through 7)  HERE:
     // PROBLEM 1: method to draw horizontal stripes
+  public static void drawHorizontalStripes(ColorGrid grid, Bridges bridges, Color color1, Color color2) throws RateLimitException, IOException {
+        int numRows = grid.getHeight();
+        int numCols = grid.getWidth();
+		Color color;
 
+        for (int row = 0; row < numRows; row++) {
+            if (row % 2 == 0) {
+    			color = color1;
+			} else {
+    			color = color2;
+			}
+            for (int col = 0; col < numCols; col++) {
+                grid.set(row, col, color);
+            }
+        }
+        bridges.setDataStructure(grid);
+        bridges.visualize();
+    }
 
 	// PROBLEM 2: method to draw vertical stripes
+	public static void drawVerticalStripes(ColorGrid grid, Bridges bridges, Color color1, Color color2) throws RateLimitException, IOException {
+        int numRows = grid.getHeight();
+        int numCols = grid.getWidth();
+		Color color;
+
+        for (int row = 0; row < numCols; row++) {
+            if (row % 2 == 0) {
+    			color = color1;
+			} else {
+    			color = color2;
+			}
+
+            for (int col = 0; col < numRows; col++) {
+                grid.set(col, row, color);
+            }
+        }
+        bridges.setDataStructure(grid);
+        bridges.visualize();
+    }
 
 
 
 	// PROBLEM 3: method to draw checkerboard
+	public static void drawCheckerboard(ColorGrid grid, Bridges bridges, Color color1, Color color2) throws RateLimitException, IOException {
+        int numRows = grid.getHeight();
+        int numCols = grid.getWidth();
+		Color color;
+
+        for (int row = 0; row < numRows; row++) {
+            for (int col = 0; col < numCols; col++) {
+				if ((row + col) % 2 == 0) {
+    				color = color1;
+				} else {
+    				color = color2;
+					}	
+
+                grid.set(row, col, color);
+            }
+        }
+        bridges.setDataStructure(grid);
+        bridges.visualize();
+    }
+
+
 
 
 
 	// PROBLEM 4: method to draw border
-
+public static void drawBorder(ColorGrid grid, Bridges bridges, Color borderColor, Color interiorColor) throws RateLimitException, IOException {
+        int numRows = grid.getHeight();
+        int numCols = grid.getWidth();
+        for (int row = 0; row < numRows; row++) {
+            for (int col = 0; col < numCols; col++) {
+                if (row == 0 || row == numRows - 1 || col == 0 || col == numCols - 1) {
+                    grid.set(row, col, borderColor);
+                } else {
+                    grid.set(row, col, interiorColor);
+                }
+            }
+        }
+        bridges.setDataStructure(grid);
+        bridges.visualize();
+    }
 
 
 	// PROBLEM 5: method to draw X
+	public static void drawX(ColorGrid grid, Bridges bridges, Color color1, Color color2) throws RateLimitException, IOException {
+		int numRows = grid.getHeight();
+		int numCols = grid.getWidth();
+		int smallerDimension;
+		if (numRows < numCols) {
+    		smallerDimension = numRows;
+		} else {
+    		smallerDimension = numCols;
+		}
+	
+		for (int i = 0; i < smallerDimension; i++) {
+			if (i % 2 == 0) {
+				grid.set(i, i, color1);
+				grid.set(numRows - 1 - i, i, color2);
+			} else {
+				grid.set(i, i, color2);
+				grid.set(numRows - 1 - i, i, color1);
+			}
+			if (i == numRows - 1 - i && numRows == numCols) { 
+				grid.set(i, i, color1);
+			}
+		}
+		bridges.setDataStructure(grid);
+		bridges.visualize();
+	}
+	
+	
+
 
 
 
 	// PROBLEM 6: method to draw diagonal split
+	public static void drawDiagSplit(ColorGrid grid, Bridges bridges, Color color1, Color color2) throws RateLimitException, IOException {
+		int numRows = grid.getHeight();
+		int numCols = grid.getWidth();
+		for (int row = 0; row < numRows; row++) {
+            for (int col = 0; col < numCols; col++) {
+                if (col <= row) {
+                    grid.set(row, col, color1);
+                } else {
+                    grid.set(row, col, color2);
+                }
+            }
+        }
+        bridges.setDataStructure(grid);
+        bridges.visualize();
+    }
 
+
+	
 
 
 	// PROBLEM 7: method to draw 4 quadrants
+	public static void drawQuadrants(ColorGrid grid, Bridges bridges, Color color1, Color color2, Color color3,Color color4) throws RateLimitException, IOException {
+	int numRows = grid.getHeight();
+	int numCols = grid.getWidth();
+	
+	int midRows = numRows/2;
+	int midCols = numCols/2;
+
+	for(int row = 0; row<numRows; row++){
+		for(int col =0; col<numCols;col++){
+			if(row<midRows && col<midCols){
+				grid.set(row,col,color1);
+			}
+			else if(row<midRows && col >= midCols){
+				grid.set(row,col,color2);
+			}
+			else if(row>=midRows && col<midCols){
+			grid.set(row,col,color3);
+			}
+			
+			else{
+				grid.set(row,col,color4);
+
+			}
+		}
+	}
+	bridges.setDataStructure(grid);
+	bridges.visualize();
+
+
+}
 
 
 
-}  // end class
+}// end class
--- Pattern Builder/PatternRunner.java
+++ Pattern Builder/PatternRunner.java
@@ -6,6 +6,7 @@ import java.io.IOException;
 import bridges.base.Color;
 import bridges.base.ColorGrid;
 
+
 public class PatternRunner {
     public static void main(String[] args) throws RateLimitException, IOException {
         // Instantiate a Bridges object with your:
@@ -20,7 +21,7 @@ public class PatternRunner {
         ColorGrid grid = new ColorGrid(14, 20);
 
         // Instantiate an object of the Pattern class called pat
-     
+     Pattern pat  = new Pattern();
 
         // CALL METHOD FOR SAMPLE PROBLEM: drawSample() sample pattern (two meager beady eyes)
         // Two arguments: your ColorGrid instance, your Bridges instance
@@ -28,27 +29,36 @@ public class PatternRunner {
         // CALL METHOD FOR PROBLEM 1: drawHorizontalStripes()  Make every other row a different color.
         // Four arguments: your ColorGrid instance, your Bridges instance, color, color)
         //pat.drawHorizontalStripes(grid, bridges, new Color("red"), new Color("yellow"));
+        pat.drawHorizontalStripes(grid, bridges, new Color("red"), new Color("yellow"));
+
 
         // CALL METHOD FOR PROBLEM 2: drawVerticalStripes() Make every other column a different color.
         // Four arguments: your ColorGrid instance, your Bridges instance, color, color)
         //pat.drawVerticalStripes(grid, bridges, new Color("blue"), new Color("red"));
+        pat.drawVerticalStripes(grid, bridges, new Color("blue"), new Color("red"));
+
 
         // CALL METHOD FOR PROBLEM 3: drawCheckerboard()  Create a checkerboard pattern
         // Four arguments: your ColorGrid instance, your Bridges instance, color, color)
-        //pat.drawCheckerboard(grid, bridges, new Color("orange"), new Color("black"));
+        //pat.drawCheckerboard(grid, bridges, new Color("orange"), new Color("black"));\
+        pat.drawCheckerboard(grid, bridges, new Color("orange"), new Color("black"));
 
         // CALL METHOD FOR PROBLEM 4: drawBorder()  Make all the border cells one color and all the interior cells another color
         // Four arguments: your ColorGrid instance, your Bridges instance, color, color)
         //pat.drawBorder(grid, bridges, new Color("yellow"), new Color("blue"));
+        pat.drawBorder(grid, bridges, new Color("yellow"), new Color("blue"));
+
 
         // CALL METHOD FOR PROBLEM 5: drawX()  Make an “X” design
         // Four arguments: your ColorGrid instance, your Bridges instance, color, color)
         // NOTE: If your grid is not square, it is okay that your "X" is not centered
         //pat.drawX(grid, bridges, new Color("blue"), new Color("yellow"));
+        pat.drawX(grid, bridges, new Color("blue"), new Color("yellow"));
 
         // CALL METHOD FOR PROBLEM 6: drawDiagSplit() Along the "slope = -1 diagonal", split the grid, one color each side
         // Four arguments: your ColorGrid instance, your Bridges instance, color, color)
         //pat.drawDiagSplit(grid, bridges, new Color("blue"), new Color("yellow"));
+        pat.drawDiagSplit(grid, bridges, new Color("blue"), new Color("yellow"));
 
         // CALL METHOD FOR PROBLEM 7: drawQuadrants()  Show the 4-quadrants in a different color for each.
         // If the ColorGrid has  an odd number of rows and an odd number of columns,
@@ -56,6 +66,6 @@ public class PatternRunner {
         // and the x and y axes will merely be suggested by the color changes
         // Six arguments: your ColorGrid instance, your Bridges instance, color, color, color, color)
         // pat.drawQuadrants(grid, bridges, new Color("blue"), new Color("yellow"), new Color("green"), new Color("red"));
-
+        pat.drawQuadrants(grid, bridges, new Color("blue"), new Color("yellow"), new Color("green"), new Color("red"));
     } // end main method
 } // end class
